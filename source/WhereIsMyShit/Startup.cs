0f66c17e847735b6a68f9f47fa2b5c1509480b86
Barry Dwyer <barry.dwyer@chillisoft.co.za>
2016-01-15 15:06:31 +0200

    Initial Commit

---
+++ source/WhereIsMyShit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WhereIsMyShit.Startup))]
+namespace WhereIsMyShit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
