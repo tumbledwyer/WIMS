904bae6591be9694132aae55c327dd5558fd8f6d
Barry Dwyer <barry.dwyer@chillisoft.co.za>
2016-01-15 16:34:37 +0200

    Added gulp

---
+++ source/WhereIsMyShit.Tests/TestHomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using WhereIsMyShit.Controllers;
+
+namespace WhereIsMyShit.Tests
+{
+    [TestFixture]
+    public class TestHomeController
+    {
+        [Test]
+        public void Index_ShouldReturnAResult()
+        {
+            //---------------Set up test pack-------------------
+            var controller = new HomeController();
+            //---------------Assert Precondition----------------
+            //---------------Execute Test ----------------------
+            var actionResult = controller.Index();
+            //---------------Test Result -----------------------
+            Assert.IsNotNull(actionResult);
+        }
+    }
+}
